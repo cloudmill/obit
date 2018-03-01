29ee372a5ad169d9b47d8b92ac577b6fec588bf7
cloudmill <34512874+cloudmill@users.noreply.github.com>
2018-03-01 14:40:20 +0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,11 @@ $(document).ready(function() {
 		$('.slides_count span').eq(0).html(i);
 		$('.slides_count span').eq(1).html(slick.slideCount);
 	});
+	$(".stocks .inner .items").on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+		var i = (currentSlide ? currentSlide : 0) + 1;
+		$('.stocks .slides_count span').eq(0).html(i);
+		$('.stocks .slides_count span').eq(1).html(slick.slideCount);
+	});
 	$('.calculator .left_cnt a').on('click', function(e) {
 		e.preventDefault();
 		var type = $(this).html();
@@ -22,6 +27,36 @@ $(document).ready(function() {
 	});
 	$('.main_slider .inner').slick({
 		fade: true
+	});
+	$('.stocks .inner .items').slick({
+		responsive: [
+		{
+			breakpoint: 2600,
+			settings: "unslick"
+		},
+		{
+			breakpoint: 1024,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				centerMode: true,
+				infinite: false
+			}
+		},
+		{
+			breakpoint: 768,
+			settings: {
+				centerMode: false,
+				infinite: false
+			}
+		}
+		]
+	});
+	$('.check_address .inputs input[name=region]').on('click', function() {
+		$(this).next().fadeToggle();
+	});
+	$('.check_address .inputs ul li').on('click', function() {
+		$(this).parent().fadeToggle();
 	});
 	$('.burger').on('click', function() {
 		$(this).toggleClass('active');
